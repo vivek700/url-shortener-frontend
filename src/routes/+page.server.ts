7ee510cdc02826af0102c41dd76856755a1bbb6d
Vivek <vs690799@gmail.com>
2024-11-25 14:00:35 +0530

    updated and added new code

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,29 @@
-import type { Actions } from "./$types"
+import type { Actions, PageServerData } from "./$types"
+
 
 export const actions = {
     default: async (event) => {
         const data = event.request.formData()
         const url = (await data).get('url')
-        console.log(url)
+        return { success: true, data: await getShortUrl(url) }
 
 
     }
 } satisfies Actions
+
+
+const getShortUrl = async (url: FormDataEntryValue | null) => {
+
+    const res = await fetch("https://url-shortener-production-1a13.up.railway.app/api/shorten", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ url: url })
+    })
+    const data = await res.json()
+    return data
+
+
+
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,34 @@
-<main >
-    <section >
-        <form method="POST">
-            <label>
-                Enter the link
-                <input name="url" type="url" />
-            </label>
+<script lang="ts">
+    import type {ActionData} from "./$types"
+    let { form}:{form: ActionData} = $props()
+    console.log(form)
 
-            <button>Shorten</button>
+</script>
 
-        </form>
-        
+<main>
+    <section class="xl:w-3/6 w-11/12 mx-auto py-28">
+        <section class="pb-8">
+            <form method="POST" class="w-full flex flex-col gap-y-5">
+                <label>
+                    <input class="py-3 px-2 text-lg rounded bg-[#343434] w-full outline-none" placeholder="Enter the link..." name="url" type="url" />
+                </label>
+                <button class="block text-lg self-center py-2 w-3/6 rounded text-black bg-[#CEB08F]">Shorten</button>
+            </form>
+        </section>
+        {#if form?.success}
+            <section class="bg-[#343434] rounded w-full p-4 text-lg text-[#ddbe9d] flex flex-col gap-y-2">
+                <p>Short url: <a href={form.data?.short_url} class="hover:underline">
+                    {form.data?.short_url}
+                </a></p>
+               
+                <p>Original url: <a href={form.data?.original_url} class="hover:underline">
+                    {form.data?.original_url}
+                </a></p>
+                <p>Short code: {form.data?.short_code}</p>
+            
+            </section>
+        {/if}
+       
     </section>
 </main>
 
